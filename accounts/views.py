a14caf410752a4247e9d3d8a805106f9f938e1eb
su12 <ts04109@naver.com>
2023-07-23 21:52:23 +0900

    로그인 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.conf import settings
 
-def login(request):
-    return render(request, 'accounts/login.html')
+signup = CreateView.as_view(
+    form_class = UserCreationForm,
+    # 기본 URL 변경
+    template_name = 'accounts/form.html',
+    # 로그인 성공시 보낼 URL
+    success_url = settings.LOGIN_URL,
+)
 
-def logout(request):
-    return render(request, 'accounts/logout.html')
+login = LoginView.as_view(
+    template_name = 'accounts/form.html',
+)
 
-def signup(request):
-    return render(request, 'accounts/signup.html')
+logout = LogoutView.as_view(
+    next_page = settings.LOGIN_URL,
+)
 
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
